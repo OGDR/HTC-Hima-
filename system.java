ec9fb17a3568a8b01493ddac0e40b8ef82a75773
OGDR <eec58impal@tutanota.com>
2022-12-23 10:42:09 -0800

    Update system.java

--- system.java
+++ system.java
@@ -1,4 +1,9 @@
- @SuppressWarnings("unused")
+ private static void arraycopy(byte[] src, int srcPos, byte[] dst, int dstPos, int length)
+   change to
+  public static void arraycopy(byte[] src, int srcPos, byte[] dst, int dstPos, int length) {
+
+
+@SuppressWarnings("unused")
     private static void arraycopy(byte[] src, int srcPos, byte[] dst, int dstPos, int length) {
         if (src == null) {
             throw new NullPointerException("src == null");
